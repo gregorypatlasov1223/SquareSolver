89eb0f6eaed947193ba3bd5cd06b281e10dbc4b0
gregorypatlasov1223 <gregory.patlasov@gmail.com>
2025-08-22 20:12:09 +0300

    outpet is added

--- Square_solver.c
+++ Square_solver.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 #include <math.h>
-void solver(double a, double b, double c, double *ptr_root1, double *ptr_root2);
-void input(double *ptr_a, double *ptr_b, double *ptr_c);
+
+void input_coef(double *ptr_a, double *ptr_b, double *ptr_c);
+int solver(double a, double b, double c, double *ptr_root1, double *ptr_root2);
+void output_results(int number_of_roots, double *ptr_root1, double *ptr_root2);
+
 int main()
 {
     double coef_a = 0, coef_b = 0, coef_c = 0;
     double root_1 = 0, root_2 = 0;
+    int nRoots = 0;
 
     printf("Let's solve an equation of the form ax2 + bx + c = 0.\n");
 
-    input(&coef_a, &coef_b, &coef_c);
+    input_coef(&coef_a, &coef_b, &coef_c);
 
     printf("Now let's move on to finding the roots.\n");
 
-    solver(coef_a, coef_b,coef_c, &root_1, &root_2);
+    nRoots = solver(coef_a, coef_b,coef_c, &root_1, &root_2);
+
+    output_results(nRoots, &root_1, &root_2);
 
     return 0;
 }
 
-void input(double *ptr_a, double *ptr_b, double *ptr_c)
+void input_coef(double *ptr_a, double *ptr_b, double *ptr_c)
 {
     printf("Enter the coefficient a.\n");
     scanf("%lf", ptr_a);
@@ -30,24 +36,24 @@ void input(double *ptr_a, double *ptr_b, double *ptr_c)
     scanf("%lf", ptr_c);
 }
 
-void solver(double a, double b, double c, double *ptr_root1, double *ptr_root2)
+int solver(double a, double b, double c, double *ptr_root1, double *ptr_root2)
 {
-    double discriminant;
+    double discriminant = 0;
 
-    if (a == 0)
+    if (fabs(a) < 0.00000001)
     {
-        if (b == 0)
+        if (fabs(b) < 0.00000001)
         {
-            if (c == 0)
-                printf("An infinite number of solutions.");
+            if (fabs(c) < 0.00000001)
+                return 1;
             else
-                printf("The equation has no solutions with the given coefficients!");
+                return 2;
         }
 
         else
         {
             *ptr_root1 = *ptr_root2 = -c/b;
-            printf("The root of the equation is %lf.\n", *ptr_root1);
+            return 3;
         }
     }
 
@@ -55,23 +61,37 @@ void solver(double a, double b, double c, double *ptr_root1, double *ptr_root2)
     {
         discriminant = b * b - 4 * a * c;
         if (discriminant < 0)
-            printf("The equation has no solutions with the given coefficients!\n");
+            return 4;
 
         else if (discriminant > 0)
         {
             *ptr_root1 = (-b + sqrt(discriminant)) / (2 * a);
             *ptr_root2 = (-b - sqrt(discriminant)) / (2 * a);
-
-            printf("The roots of the equation are numbers %lf and %lf.\n", *ptr_root1, *ptr_root2);
+            return 5;
         }
 
         else
         {
             *ptr_root1 = *ptr_root2 = -b / (2 * a);
-            printf("The equation has a single root = %lf.\n", *ptr_root1);
+            return 6;
         }
 
      }
+}
+
+void output_results(int number_of_roots, double *ptr_root1, double *ptr_root2)
+{
+    if (number_of_roots == 2 || number_of_roots == 4)
+        printf("The equation has no solutions with the given coefficients!");
+
+    if (number_of_roots == 3 || number_of_roots == 6)
+        printf("The root of the equation is %lf.\n", *ptr_root1);
+
+    if (number_of_roots == 5)
+        printf("The roots of the equation are numbers %lf and %lf.\n", *ptr_root1, *ptr_root2);
+
+    if (number_of_roots == 1)
+        printf("An infinite number of solutions.");
 }
 
 
